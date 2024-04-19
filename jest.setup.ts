65fffc9c4d22d00dbaffb27efa5aac45ed66ff8c
Eason Chang <eason@easonchang.com>
2024-04-19 01:05:03 -0600

    test: Mock next/navigation

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,14 @@
 import '@testing-library/jest-dom';
+
+import { useRouter } from 'next-router-mock';
+
+jest.mock('next/navigation', () => {
+  const usePathname = () => {
+    const router = useRouter();
+    return router.pathname;
+  };
+  return {
+    useRouter,
+    usePathname,
+  };
+});
