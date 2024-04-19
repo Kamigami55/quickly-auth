a846197e7389bfb6effcb1c36e5b12ed4894572b
Eason Chang <eason@easonchang.com>
2024-04-18 21:11:35 -0600

    feat: Refine profile page UI

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 
+import { CircleUser } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useAuth } from '@/hooks/useAuth';
 
 export default function ProfilePage() {
@@ -31,17 +27,76 @@ export default function ProfilePage() {
         <Card>
           <CardHeader>
             <CardTitle>User</CardTitle>
-            <CardDescription>Your user information.</CardDescription>
           </CardHeader>
-          <CardContent>{JSON.stringify(profile?.user)}</CardContent>
+          <CardContent>
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarFallback>
+                  <CircleUser className="h-5 w-5" />
+                </AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-medium leading-none">
+                  {profile?.user?.first_name} {profile?.user?.last_name}
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  {profile?.user?.email}
+                </p>
+              </div>
+            </div>
+          </CardContent>
         </Card>
 
         <Card>
           <CardHeader>
             <CardTitle>Company</CardTitle>
-            <CardDescription>Your company information.</CardDescription>
           </CardHeader>
-          <CardContent>{JSON.stringify(profile?.company)}</CardContent>
+          <CardContent className="grid gap-4">
+            <div className="grid grid-cols-2 gap-2">
+              <div className="text-sm text-gray-500">Legal Name:</div>
+              <div className="text-sm">{profile?.company?.legal_name}</div>
+            </div>
+            <div className="grid grid-cols-2 gap-2">
+              <div className="text-sm text-gray-500">Business Type:</div>
+              <div className="text-sm">{profile?.company?.business_type}</div>
+            </div>
+            <div className="grid grid-cols-2 gap-2">
+              <div className="text-sm text-gray-500">Industry:</div>
+              <div className="text-sm">{profile?.company?.industry}</div>
+            </div>
+            <div className="grid grid-cols-2 gap-2">
+              <div className="text-sm text-gray-500">Website:</div>
+              <div className="text-sm">{profile?.company?.website}</div>
+            </div>
+            <div className="grid grid-cols-2 gap-2">
+              <div className="text-sm text-gray-500">
+                Business Registration:
+              </div>
+              <div className="text-sm">
+                {profile?.company?.business_registration}
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-2">
+              <div className="text-sm text-gray-500">Phone:</div>
+              <div className="text-sm">{profile?.company?.phone}</div>
+            </div>
+            <div className="grid grid-cols-2 gap-2">
+              <div className="text-sm text-gray-500">Business Number:</div>
+              <div className="text-sm">{profile?.company?.business_number}</div>
+            </div>
+            <div className="grid grid-cols-2 gap-2">
+              <div className="text-sm text-gray-500 ">Expected Activity:</div>
+              <div className="text-sm">
+                {profile?.company?.expected_activity}
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-2">
+              <div className="text-sm text-gray-500 ">Early Pay Intent:</div>
+              <div className="text-sm">
+                {profile?.company?.early_pay_intent ? 'Yes' : 'No'}
+              </div>
+            </div>
+          </CardContent>
         </Card>
       </div>
     </div>
